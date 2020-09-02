f1d013f0439c974688864e0900ef48d679dfbcf1
daniel44 <daniel.linares@ciencias.unam.mx>
2020-09-02 10:48:59 -0500

    BUG FIX | imagew url incorrectly set | Listing images directory now is inside static directory

--- auctions/models.py
+++ auctions/models.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.conf import settings
 
 
 class User(AbstractUser):
@@ -18,9 +19,10 @@ class Listing(models.Model):
                                related_name='listings')
     title = models.CharField(max_length=24)
     description = models.CharField(max_length=128)
-    image = models.ImageField(upload_to=f'listing_images/{title}',
-                              blank=True,
-                              null=True)
+    image = models.ImageField(
+        upload_to=f'{settings.STATIC_URL}/listing_images/{title}',
+        blank=True,
+        null=True)
     initial_bid_amount = models.DecimalField(max_digits=12, decimal_places=2)
     actual_bid = models.OneToOneField('Bid',
                                       on_delete=models.PROTECT,
