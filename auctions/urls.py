5958d85bfb35364fb3b81ac92cf850d9cb7e2a79
daniel44 <daniel.linares@ciencias.unam.mx>
2020-09-04 12:19:19 -0500

    FEATURE / UNCOMPLETED | Urls for listings spec

--- auctions/urls.py
+++ auctions/urls.py
@@ -9,4 +9,12 @@ urlpatterns = [
     path("logout", views.logout_view, name="logout"),
     path("register", views.register, name="register"),
     path("create_listing", views.create_listing, name="create_listing"),
+    path("listing/<int:listing_id>", views.listing, name="listing"),
+    path("listing/<int:listing_id>/bid", views.bid, name="bid"),
+    path("listing/<int:listing_id>/toogle_watchlist",
+         views.toogle_watchlist,
+         name="toogle_watchlist"),
+    path("listing/<int:listing_id>/close",
+         views.close_listing,
+         name="close_listing"),
 ]
